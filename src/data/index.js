1d5b6f7748e4384080f5a676f06dcf5136adafbe
faran4engg <faran4engg@gmail.com>
2022-03-12 21:32:30 +0100

    chore: cleanup

--- src/data/index.js
+++ src/data/index.js
@@ -1,64 +1,62 @@
-// import { Img01, Img02, Img03, Img04, Img05, Img06 } from "../assets";
-
 export const cards = [
   {
     id: 1,
-    matchingId: 2,
+    matchingId: "a",
     logo: "https://cdn.discordapp.com/attachments/935485323490766948/952279872728891402/ilya-skaletta-3Ou6F7f48yo-unsplash.jpg",
   },
   {
     id: 2,
-    matchingId: 1,
+    matchingId: "a",
     logo: "https://cdn.discordapp.com/attachments/935485323490766948/952279872728891402/ilya-skaletta-3Ou6F7f48yo-unsplash.jpg",
   },
   {
     id: 3,
-    matchingId: 4,
+    matchingId: "b",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279872439455814/peter-golob-4KtpdFMvkNU-unsplash.jpg",
   },
   {
     id: 4,
-    matchingId: 3,
+    matchingId: "b",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279872439455814/peter-golob-4KtpdFMvkNU-unsplash.jpg",
   },
   {
     id: 5,
-    matchingId: 6,
+    matchingId: "c",
     logo: "https://cdn.discordapp.com/attachments/935485323490766948/952279804982472764/lse-library-Mx1sghCziHo-unsplash.jpg",
   },
   {
     id: 6,
-    matchingId: 5,
+    matchingId: "c",
     logo: "https://cdn.discordapp.com/attachments/935485323490766948/952279804982472764/lse-library-Mx1sghCziHo-unsplash.jpg",
   },
   {
     id: 7,
-    matchingId: 8,
+    matchingId: "d",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279804676276275/the-new-york-public-library-Y9MoiZi9Rbg-unsplash.jpg",
   },
   {
     id: 8,
-    matchingId: 7,
+    matchingId: "d",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279804676276275/the-new-york-public-library-Y9MoiZi9Rbg-unsplash.jpg",
   },
   {
     id: 9,
-    matchingId: 10,
+    matchingId: "e",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279804357533767/lse-library-ddNkplRhzv0-unsplash.jpg",
   },
   {
     id: 10,
-    matchingId: 9,
+    matchingId: "e",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279804357533767/lse-library-ddNkplRhzv0-unsplash.jpg",
   },
   {
     id: 11,
-    matchingId: 12,
+    matchingId: "f",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279519446855680/stoica-ionela-LPiMaZ00gPU-unsplash.jpg",
   },
   {
     id: 12,
-    matchingId: 11,
+    matchingId: "f",
     logo: "https://media.discordapp.net/attachments/935485323490766948/952279519446855680/stoica-ionela-LPiMaZ00gPU-unsplash.jpg",
   },
 ];
--- src/utils/shuffleArr.js
+++ src/utils/shuffleArr.js
@@ -4,33 +4,3 @@ export const shuffleArr = (arr) => {
     .sort((a, b) => a.sortKey - b.sortKey)
     .map(({ item }) => item);
 };
-
-var arr = [1, 2, 3];
-
-// .map((item) => ({ item, sortKey: Math.random() }))
-arr = [
-  {
-    1: 1,
-    sortKey: 0.1231,
-  },
-  {
-    2: 2,
-    sortKey: 0.4534,
-  },
-  {
-    3: 3,
-    sortKey: 0.867,
-  },
-];
-
-arr = [
-  {
-    1: 1,
-  },
-  {
-    2: 2,
-  },
-  {
-    3: 3,
-  },
-];
